03bee1957db6eaac233a7f2a1350f59eae9d9289
NatalyK <natalyak.qa@gmail.com>
2024-10-10 21:29:08 -0400

    Homework 3 corrected

--- homework/natalya_kamynina/Homework_3/task_3.py
+++ homework/natalya_kamynina/Homework_3/task_3.py
@@ -1,10 +1,10 @@
 a = 4
 b = 2
 
-calc_arf = (a+b)/2
+calc_arf = (a + b) / 2
 print(calc_arf)
 
 print("=======================================")
 
-calc_geom = (a*b) ** 0.5
+calc_geom = (a * b) ** 0.5
 print(calc_geom)
