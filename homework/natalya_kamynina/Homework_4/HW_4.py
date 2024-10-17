c2a2d98c3915e25695e0edf50471ac07489ed0d1
NatalyK <natalyak.qa@gmail.com>
2024-10-17 10:50:35 -0400

    Homework_4 done v.1

--- homework/natalya_kamynina/Homework_4/HW_4.py
+++ homework/natalya_kamynina/Homework_4/HW_4.py
@@ -11,30 +11,23 @@ my_dict = {
     'set': {1, 2, 3, 4, 5}
 }
 
-
-
-
 print(my_dict['tuple'][-1])
 print('-' * 100)
-
 
 my_dict['list'].append('88')
 my_dict['list'].pop(1)
 print(my_dict['list'])
 print('-' * 100)
 
-
 my_dict['dict']['i am a tuple'] = '2'
 del my_dict['dict']['5']
 print(my_dict['dict'])
 print('-' * 100)
 
-
 my_dict['set'].add(888)
 my_dict['set'].remove(5)
 print(my_dict['set'])
 print('-' * 100)
-
 
 print(my_dict)
 print('-' * 100)
