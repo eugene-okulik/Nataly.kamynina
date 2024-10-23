c054c0f18544b59ec4c2932ede1936dc877a35b2
NatalyK <natalyak.qa@gmail.com>
2024-10-23 09:36:40 -0400

    Homework 5 updated

--- homework/natalya_kamynina/Homework_5/Task_2.py
+++ homework/natalya_kamynina/Homework_5/Task_2.py
@@ -1,12 +1,12 @@
 aaa = 'результат операции: 42'
-print(aaa.index('е') + 10)
+print(aaa.index(' ') + 10)
 
 print('_' * 100)
 
 bbb = 'результат операции: 514'
-print(bbb.index('5') + 10)
+print(bbb.index(' ') + 10)
 
 print('_' * 100)
 
 ccc = 'результат работы программы: 9'
-print(ccc.index('з') + 10)
+print(ccc.index(' ') + 10)
