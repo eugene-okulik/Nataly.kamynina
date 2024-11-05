a63aafe8dcae3cdb0116c24f5e90d6e982aae065
NatalyK <natalyak.qa@gmail.com>
2024-11-04 22:15:38 -0500

    Homework 7, Lint errors fixed

--- homework/natalya_kamynina/Homework_7/Task_2.py
+++ homework/natalya_kamynina/Homework_7/Task_2.py
@@ -2,7 +2,7 @@ words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 for key in words:
     if key.startswith('I'):
         print(key * words['I'])
-    elif  key.startswith('l'):
+    elif key.startswith('l'):
         print(key * words['love'])
     elif key.startswith('P'):
         print(key)
--- homework/natalya_kamynina/Homework_7/Task_3.py
+++ homework/natalya_kamynina/Homework_7/Task_3.py
@@ -1,7 +1,8 @@
-def task_3(my_string:str, index:int):
+def task_3(my_string: str, index: int):
     words = my_string.split()
-    result = int(words[index])+ 10
+    result = int(words[index]) + 10
     return result
+
 
 print(task_3('результат операции: 42', 2))
 print(task_3('результат операции: 54', 2))
