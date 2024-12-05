4cc93aa395420578e9a1e03ab056922eb498a696
NatalyK <natalyak.qa@gmail.com>
2024-12-05 11:37:22 -0500

    Homework 9 Lint fixed

--- homework/natalya_kamynina/Homework_9/Task_1.py
+++ homework/natalya_kamynina/Homework_9/Task_1.py
@@ -1,7 +1,8 @@
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
-                29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+
 def is_hot_days(x):
     return x > 28
+
 temperatures = filter(is_hot_days, temperatures)
 new_list = (list(temperatures))
 max_temp = max(new_list)
@@ -14,3 +15,4 @@ print('==' * 50)
 print(f"max temp: {max_temp}")
 print(f"min temp: {min_temp}")
 print(f"average temp: {round(aver_temp)}")
+
